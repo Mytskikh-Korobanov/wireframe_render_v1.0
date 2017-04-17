a4138db63361f3f33598a216d936ea460e4301e8
Mytskikh-Korobanov <mytskikh@list.ru>
2017-04-18 01:25:29 +0300

    Create rot_basis_in_2d (15).pas

---
+++ central/animation/rot_basis_in_2d (15).pas
@@ -0,0 +1,25 @@
+program rot_basis_in_2d;
+// вращение базиса по двум осям
+// © Мыцких-Коробанов А. Ю., 2017
+
+uses
+  geom_obj, GraphABC, ABCObjects, Timers {привязка к PascalABC};
+
+var
+  count: real;
+
+begin
+  wind(400, 400, 'Вращение базиса по двум осям');
+  camera(150, 150, 150, pi/4, 1, 2, -1, -1, -1);
+  LockDrawingObjects;
+  LockDrawing;
+  count := 0;
+  while true do
+  begin
+    draw_basis(0, 0, 0, 0, count, count, 1, 1, 1, 50, 50, 50);
+    count := count + pi/16;
+    RedrawObjects;
+    Sleep(200);
+    window.Clear;
+  end;
+end.
